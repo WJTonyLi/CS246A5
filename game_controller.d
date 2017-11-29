88e5b258337f7d7aa2e6c4f937817fb52f39f63d
Amos Law <amos.law98@gmail.com>
2017-11-28 23:22:23 -0500

    Game loop works

--- game_controller.d
+++
@@ -1,2 +0,0 @@
-game_controller.o: game_controller.cc game_controller.h game_state.h \
- player.h abstract_card.h abstract_minion_card.h subject.h observer.h
--- game_state.d
+++
@@ -1,2 +0,0 @@
-game_state.o: game_state.cc game_state.h player.h abstract_card.h \
- abstract_minion_card.h subject.h observer.h

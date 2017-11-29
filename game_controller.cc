99147e81a0241147129f7171354c4d6844a414fc
Amos Law <amos.law98@gmail.com>
2017-11-28 23:27:35 -0500

    Fixed another typo

--- game_controller.cc
+++ game_controller.cc
@@ -18,68 +18,68 @@ void GameController::notify(Subject<std::string> &whoFrom){
             gameState->setCurrentStatus(CurrentStatus::SHOW_BOARD);
         }
         gameState->endTurn();
-    }
-
-    istringstream iss(whoFrom.getInfo());
-    string command;
-    iss >> command;
-    if (command == "help") {
-        cout << command << endl;
-    } else if (command == "end") {
-        cout << command << endl;
-    } else if (command == "quit") {
-        cout << command << endl;
-    } else if (command == "draw") {
-        cout << command << endl;
-    } else if (command == "discard") {
-        int i;
-        if (iss >> i) {
-            cout << command << " " << i << endl;
-        } else {
-            // throw invalidCommand
-        }
-    } else if (command == "attack") {
-        int i;
-        if (iss >> i) {
-            int j;
-            if (iss >> j) {
-                cout << command << " " << i << " " << j <<  endl;
-            } else {
-                cout << command << " " << i << endl;
-            }
-        } else {
-            // throw invalidCommand
-        }
-    } else if (command == "play") {
-        int i;
-        if (iss >> i) {
-            int p, t;
-            if (iss >> p && iss >> t) {
-                cout << command << " " << i << " " << p << " " << t << endl;
-            } else {
-                cout << command << " " << i << endl;
-            }
-        } else {
-            // throw invalidCommand
-        }
-    } else if (command == "use") {
-        int i;
-        if (iss >> i) {
-            int j;
-            if (iss >> j) {
-                cout << command << " " << i << " " << j <<  endl;
-            } else {
-                cout << command << " " << i << endl;
-            }
-        } else {
-            // throw invalidCommand
-        }
-    } else if (command == "hand") {
-        cout << command << endl;
-    } else if (command == "board") {
-        gameState->renderNow();
     } else {
-        // throw invalidCommand
+        istringstream iss(whoFrom.getInfo());
+        string command;
+        iss >> command;
+        if (command == "help") {
+            cout << command << endl;
+        } else if (command == "end") {
+            cout << command << endl;
+        } else if (command == "quit") {
+            cout << command << endl;
+        } else if (command == "draw") {
+            cout << command << endl;
+        } else if (command == "discard") {
+            int i;
+            if (iss >> i) {
+                cout << command << " " << i << endl;
+            } else {
+                // throw invalidCommand
+            }
+        } else if (command == "attack") {
+            int i;
+            if (iss >> i) {
+                int j;
+                if (iss >> j) {
+                    cout << command << " " << i << " " << j <<  endl;
+                } else {
+                    cout << command << " " << i << endl;
+                }
+            } else {
+                // throw invalidCommand
+            }
+        } else if (command == "play") {
+            int i;
+            if (iss >> i) {
+                int p, t;
+                if (iss >> p && iss >> t) {
+                    cout << command << " " << i << " " << p << " " << t << endl;
+                } else {
+                    cout << command << " " << i << endl;
+                }
+            } else {
+                // throw invalidCommand
+            }
+        } else if (command == "use") {
+            int i;
+            if (iss >> i) {
+                int j;
+                if (iss >> j) {
+                    cout << command << " " << i << " " << j <<  endl;
+                } else {
+                    cout << command << " " << i << endl;
+                }
+            } else {
+                // throw invalidCommand
+            }
+        } else if (command == "hand") {
+            cout << command << endl;
+        } else if (command == "board") {
+            gameState->renderNow();
+        } else {
+            // throw invalidCommand
+        }
     }
 }
 

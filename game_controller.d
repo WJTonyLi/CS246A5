481fd6bf5ae09602345454c7a55db57c14561a5c
Amos Law <amos.law98@gmail.com>
2017-11-28 23:33:57 -0500

    Skeleton game loop

---
+++ game_controller.d
@@ -0,0 +1,2 @@
+game_controller.o: game_controller.cc game_controller.h game_state.h \
+ player.h abstract_card.h abstract_minion_card.h subject.h observer.h
---
+++ main.d
@@ -0,0 +1,2 @@
+main.o: main.cc cli_view.h subject.h observer.h game_state.h player.h \
+ abstract_card.h abstract_minion_card.h game_controller.h text_view.h

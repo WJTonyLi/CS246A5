b01450a57de0a0f0d9d7b40b448f50789ef34372
Amos Law <amos.law98@gmail.com>
2017-12-01 13:19:34 -0500

    Temporary exception handling fix

--- activated_effect.d
+++
@@ -1,2 +0,0 @@
-activated_effect.o: activated_effect.cc activated_effect.h game_state.h \
- player.h subject.h observer.h
--- player.d
+++
@@ -1,4 +0,0 @@
-player.o: player.cc player.h spell_card.h abstract_card.h \
- ascii_graphics.h activated_effect.h game_state.h subject.h observer.h \
- base_minion_card.h triggered_effect.h abstract_minion_card.h \
- Effects/blizzard_effect.h Effects/../activated_effect.h

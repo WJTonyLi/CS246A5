888a5124c354e4c2185311760465542a1e5a740e
Amos Law <amos.law98@gmail.com>
2017-12-02 22:58:09 -0500

    Players can win

--- base_minion_card.d
+++
@@ -1,4 +0,0 @@
-base_minion_card.o: base_minion_card.cc base_minion_card.h \
- activated_effect.h game_state.h player.h subject.h observer.h \
- triggered_effect.h abstract_minion_card.h abstract_card.h \
- ascii_graphics.h
--- game_controller.d
+++
@@ -1,2 +0,0 @@
-game_controller.o: game_controller.cc game_controller.h game_state.h \
- player.h subject.h observer.h
